a0046c68942ccddba92edc0132f1ed67295d980e
Aymone Kouame <34042185+AymoneKouame@users.noreply.github.com>
2025-03-27 18:35:27 -0500

    Update install_if_not_installed.py

--- install_if_not_installed.py
+++ install_if_not_installed.py
@@ -1,10 +1,9 @@
-import pkg_resources
 import os
-
+import subprocess
+import pkg_resources
 def install_if_not_installed(modules):  
+    uninstalled_packages = [p for p in modules if p not in sorted(["%s==%s" % (i.key, i.version) for i in pkg_resources.working_set])]
+    uninstalled_packages_f = " ".join(uninstalled_packages)
+    subprocess.run(['pip', 'install', '--upgrade', uninstalled_packages_f])
 
-  uninstalled_packages = [p for p in modules if p not in sorted(["%s==%s" % (i.key, i.version) for i in pkg_resources.working_set])]
-  uninstalled_packages_f = " ".join(uninstalled_packages)
-  os.system(f'pip install --upgrade {uninstalled_packages_f}')
-   
 #intall_if_not_installed(modules = ['openpyxl', 'seaborn'])
